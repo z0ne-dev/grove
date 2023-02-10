89a376a5b4fc7a6a58a7d2207b853ad4f92c4b44
Ch. König <me@kurabloodlust.eu>
2023-02-10 21:35:36 +0100

    config model

---
+++ internal/db/config.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/rs/xid"
+)
+
+type Config struct {
+	Id     xid.ID
+	UserId xid.ID
+	Value  json.RawMessage
+}
+
+func ConfigForApplication(ctx context.Context, pg *pgxpool.Conn) (*Config, error) {
+	return ConfigByUserId(ctx, pg, xid.NilID())
+}
+
+func ConfigByUserId(ctx context.Context, pg *pgxpool.Conn, id xid.ID) (*Config, error) {
+	config := new(Config)
+	err := pgxscan.Get(ctx, pg, config, "SELECT * FROM config c WHERE c.user_id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
---
+++ internal/db/config_application.go
@@ -0,0 +1,5 @@
+package db
+
+type ConfigPayloadApplication struct {
+	Name string `json:"name" default:"Grove"`
+}
