a042d0a4531a1e36616329be5cbf686a0507f816
Ch. König <me@kurabloodlust.eu>
2023-02-17 20:36:07 +0100

    chore: Add create_migration script

--- scripts/create_migration.sh
+++ scripts/create_migration.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+#
+# create_migration.sh Copyright (c) 2023 z0ne.
+# All Rights Reserved.
+# Licensed under the EUPL 1.2 License.
+# See LICENSE the project root for license information.
+#
+# SPDX-License-Identifier: EUPL-1.2
+#
+
 set -e
 
 name=$1
